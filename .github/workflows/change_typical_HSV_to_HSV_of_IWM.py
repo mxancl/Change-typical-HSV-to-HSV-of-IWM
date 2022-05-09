197465aaddcc414534dc390a3231ae67a7856f56
mxancl <70007204+mxancl@users.noreply.github.com>
2022-05-09 19:44:52 +0800

    Update change_typical_HSV_to_HSV_of_IWM.py

--- .github/workflows/change_typical_HSV_to_HSV_of_IWM.py
+++ .github/workflows/change_typical_HSV_to_HSV_of_IWM.py
@@ -1,1 +1,12 @@
-
+print("use this website to get H,S,V: https://c.runoob.com/front-end/868/")
+print("to use this program, you need to install Python and search IDLE in Search bar, open it, than click File and choose Open... click this file and press F5.") 
+H,S,V=(input("input H,S,V in order,the result successively is H,S,V in IWM, use one space to split H,S and V").split())
+H=float(H)
+S=float(S)
+V=float(V)
+h=round(H*180)
+t=S*4
+w=round(V*100)
+from decimal import Decimal
+s=Decimal(t).quantize(Decimal("0.01"),rounding="ROUND_HALF_UP")
+print(h,s,'{:.0%}'.format(V))
